d9917b8aaa7a52fdc1b69ed0d3a74822ba65627d
Zouhair BERRADA <berrada902@gmail.com>
2019-10-04 17:53:15 +0200

    refacto implementation : use same method for all numbers

--- src/main/java/com/netseenergy/kata/Yatzy.java
+++ src/main/java/com/netseenergy/kata/Yatzy.java
@@ -4,6 +4,12 @@ import java.util.stream.IntStream;
 
 public class Yatzy {
 
+    private int[] dices;
+
+    public Yatzy(int d1, int d2, int d3, int d4, int d5) {
+        dices = new int[]{d1, d2, d3, d4, d5};
+    }
+
     public static int chance(int d1, int d2, int d3, int d4, int d5) {
         return IntStream.of(new int[]{d1, d2, d3, d4, d5}).sum();
     }
@@ -15,76 +21,37 @@ public class Yatzy {
         return countDistinctElements == 1 ? 50 : 0;
     }
 
-    public static int ones(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 1) sum++;
-        if (d2 == 1) sum++;
-        if (d3 == 1) sum++;
-        if (d4 == 1) sum++;
-        if (d5 == 1)
-            sum++;
+    private static int sumSameDices(int seekedNumber, int... dices) {
+        return (int)IntStream.of(dices)
+                .filter(dice -> dice == seekedNumber)
+                .count() * seekedNumber;
+    }
 
-        return sum;
+    public static int ones(int d1, int d2, int d3, int d4, int d5) {
+        return sumSameDices(1, d1, d2, d3, d4, d5);
     }
 
     public static int twos(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 2) sum += 2;
-        if (d2 == 2) sum += 2;
-        if (d3 == 2) sum += 2;
-        if (d4 == 2) sum += 2;
-        if (d5 == 2) sum += 2;
-        return sum;
+        return sumSameDices(2, d1, d2, d3, d4, d5);
     }
 
     public static int threes(int d1, int d2, int d3, int d4, int d5) {
-        int s;
-        s = 0;
-        if (d1 == 3) s += 3;
-        if (d2 == 3) s += 3;
-        if (d3 == 3) s += 3;
-        if (d4 == 3) s += 3;
-        if (d5 == 3) s += 3;
-        return s;
-    }
-
-    protected int[] dice;
-
-    public Yatzy(int d1, int d2, int d3, int d4, int _5) {
-        dice = new int[5];
-        dice[0] = d1;
-        dice[1] = d2;
-        dice[2] = d3;
-        dice[3] = d4;
-        dice[4] = _5;
+        return sumSameDices(3, d1, d2, d3, d4, d5);
     }
 
     public int fours() {
-        int sum;
-        sum = 0;
-        for (int at = 0; at != 5; at++) {
-            if (dice[at] == 4) {
-                sum += 4;
-            }
-        }
-        return sum;
+        //I don't see any benefit in neither having this method as a nonstatic nor having dices field !
+        return sumSameDices(4, dices);
     }
 
     public int fives() {
-        int s = 0;
-        int i;
-        for (i = 0; i < dice.length; i++)
-            if (dice[i] == 5)
-                s = s + 5;
-        return s;
+        //I don't see any benefit in neither having this method as a nonstatic nor having dices field !
+        return sumSameDices(5, dices);
     }
 
     public int sixes() {
-        int sum = 0;
-        for (int at = 0; at < dice.length; at++)
-            if (dice[at] == 6)
-                sum = sum + 6;
-        return sum;
+        //I don't see any benefit in neither having this method as a nonstatic nor having dices field !
+        return sumSameDices(6, dices);
     }
 
     public static int score_pair(int d1, int d2, int d3, int d4, int d5) {
